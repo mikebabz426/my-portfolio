203373bd0001044d958b1370c5ed5355f8406597
mikebabz426 <60781512+mikebabz426@users.noreply.github.com>
2020-11-13 17:04:04 -0600

    add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,25 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Container, Typography, Grid } from "@material-ui/core"
+import {
+  Container,
+  Typography,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import ContactMeSvg from "../assets/contact.svg"
+import * as Yup from "yup"
+import { Formik, Form, Field } from "formik"
+
+//Form Validation:
+
+let contactSchema = Yup.object().shape({
+  name: Yup.string().required().min(3),
+  email: Yup.string().email().required(),
+  message: Yup.string().required(),
+})
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -36,22 +52,87 @@ const ContactPage = () => {
           align="center"
         >
           <Grid item md={6}>
+            <Typography variant="h5" style={{ marginBottom: "1rem" }}>
+              Need help with your next project?
+            </Typography>
             <Typography variant="h3">Don't be shy, lets talk!</Typography>
             <ContactMeSvg className={classes.svg} />
           </Grid>
           <Grid item md={6}>
-            <Typography variant="h1">About</Typography>
-            <Typography
-              variant="body1"
-              style={{ marginTop: "2rem", textAlign: "justify" }}
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                message: "",
+              }}
+              validationSchema={contactSchema}
+              // onSubmit={values => {}}
             >
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa,
-              consectetur. Sapiente sed ut veniam, rerum nesciunt fugit dolore
-              dicta, voluptate minima, aliquam unde. Consequuntur repudiandae
-              veritatis ipsam, omnis quas sed. Omnis illo nostrum, beatae
-              voluptates in optio velit neque architecto corrupti amet
-              inventore. Quos eos blanditiis dolores eum veritatis amet.
-            </Typography>
+              {({ errors, touched }) => {
+                return (
+                  <Form>
+                    <Field
+                      name="name"
+                      type="input"
+                      variant="outlined"
+                      margin="normal"
+                      label="Your Name"
+                      required
+                      fullWidth
+                      as={TextField}
+                    />
+                    {errors.name && touched.name ? (
+                      <Typography color="error">
+                        Please enter a valid name
+                      </Typography>
+                    ) : null}
+                    <Field
+                      name="email"
+                      type="input"
+                      variant="outlined"
+                      margin="normal"
+                      label="Your Email"
+                      required
+                      fullWidth
+                      as={TextField}
+                    />
+                    {errors.email && touched.email ? (
+                      <Typography color="error">
+                        Please enter a valid email address
+                      </Typography>
+                    ) : null}
+                    <Field
+                      name="message"
+                      type="input"
+                      variant="outlined"
+                      margin="normal"
+                      label="Your message"
+                      required
+                      fullWidth
+                      multiline
+                      rowsMax={5}
+                      as={TextField}
+                    />
+                    {errors.message && touched.message ? (
+                      <Typography color="error">
+                        Please enter a message
+                      </Typography>
+                    ) : null}
+
+                    <Button
+                      style={{ marginTop: "2rem" }}
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
+                    >
+                      Send
+                    </Button>
+                  </Form>
+                )
+              }}
+            </Formik>
           </Grid>
         </Grid>
       </Container>
